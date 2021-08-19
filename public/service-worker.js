ca1ed025c23f697b1307ed1fd26132626b4282ca
EajayD <edelos1524@hotmail.com>
2021-08-19 16:53:54 -0700

    added manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,9 +3,10 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/style.css",
     "/index.js",
+    "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
   ];
   
-  const CACHE_NAME = "static-cache-v2";
+  const CACHE_NAME = "static-cache-v1";
   const DATA_CACHE_NAME = "data-cache-v1";
